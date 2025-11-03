279a6c437dfb1d53a23942486cae5a22d9b95144
impphilwebs <imphilwebs@gmail.com>
2025-11-03 14:42:16 +0100

    Removed react-custom logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from './pages/home'
 import About from './pages/about'
 import Service from './pages/services'
